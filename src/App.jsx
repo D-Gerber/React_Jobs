0741835e0371bde9ff705e6e516bae87b0809d39
Daniel <drgerber69@gmail.com>
2024-09-06 16:10:03 +0200

    Stared doing mock backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,27 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import HomeCards from "./components/HomeCards";
-import JobListings from "./components/JobListings";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import HomePage from "./pages/HomePage";
+import JobsPage from "./pages/JobsPage";
+import NotFoundPage from "./pages/NotFoundPage";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path="/jobs" element={<JobsPage />} />
+      <Route path="*" element={<NotFoundPage />} />{" "}
+      {/* using the asterisks is like a catch all meaning if there is any page not found in our website them the 404 will show */}
+    </Route>
+  )
+);
 
 const App = () => {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <HomeCards />
-      <JobListings />
-
-      {/* <!-- Browse Jobs --> */}
-
-      <section className="m-auto max-w-lg my-10 px-6">
-        <a
-          href="jobs.html"
-          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >
-          View All Jobs
-        </a>
-      </section>
-    </>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
---
+++ src/components/JobListing.jsx
@@ -0,0 +1,53 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { FaMapMarker } from "react-icons/fa";
+
+const JobListing = ({ job }) => {
+  const [showFullDescription, setShowFullDescription] = useState(false); // declaring useState so we can change the state of the description
+
+  let description = job.description; // use let variable because it can change with each job
+
+  if (!showFullDescription) {
+    //that is an if/not ternary...if its not showing full description then run the below code.
+    description = description.substring(0, 90) + "..."; // this cuts the wording to Link max of 90 characters and adds (concatenates +) the ellipse (...) to the end. Substring cuts it down
+  }
+
+  return (
+    <div className="bg-white rounded-xl shadow-md relative">
+      <div className="p-4">
+        <div className="mb-6">
+          <div className="text-gray-600 my-2">{job.type}</div>
+          <h3 className="text-xl font-bold">{job.title}</h3>
+        </div>
+
+        <div className="mb-5">{description}</div>
+
+        <button
+          onClick={() => setShowFullDescription((precState) => !precState)}
+          className="text-indigo-500 mb-5 hover:text-indigo-600"
+        >
+          {showFullDescription ? "Less" : "More"}
+        </button>
+
+        <h3 className="text-indigo-500 mb-2">{job.salary} / Year</h3>
+
+        <div className="border border-gray-100 mb-5"></div>
+
+        <div className="flex flex-col lg:flex-row justify-between mb-4">
+          <div className="text-orange-700 mb-3">
+            <FaMapMarker className="inline text-lg mb-1 mr-1" />
+            {job.location}
+          </div>
+          <Link
+            to={`/job/${job.id}`}
+            className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
+          >
+            Read More
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default JobListing;

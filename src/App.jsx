1ead907945e77f67a17526c2ca4b26c586b0ab49
Daniel <drgerber69@gmail.com>
2024-09-11 13:02:06 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,15 @@ import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
 import JobsPage from "./pages/JobsPage";
 import NotFoundPage from "./pages/NotFoundPage";
+import JobPage from "./pages/JobPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path="/jobs" element={<JobsPage />} />
+      <Route path="/jobs/:id" element={<JobPage />} />{" "}
+      {/* the colon signifies that itt is dynamic  */}
       <Route path="*" element={<NotFoundPage />} />{" "}
       {/* using the asterisks is like a catch all meaning if there is any page not found in our website them the 404 will show */}
     </Route>
